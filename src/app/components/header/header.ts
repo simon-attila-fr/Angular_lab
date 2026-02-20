983a22bb8ffbb47f81ce27fd88341ec718f14ad5
Attila SIMON <attila.simon2025@campus-eni.fr>
2026-02-20 12:25:24 +0100

    Card, Header and UserHome style

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,6 @@ import { RouterLink } from "@angular/router";
   styleUrl: './header.css',
 })
 export class Header {
-
+  auth = inject(AuthService);
+  userAuthenticated = this.auth.userAuthenticated;
 }
